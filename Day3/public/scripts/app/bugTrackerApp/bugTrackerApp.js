db105c6f4439a42c32aba4f516a849e201215f34
Siddalingesha D S <siddhudev813@gmail.com>
2016-07-22 15:23:23 +0530

    Routing - Part - 1

--- Day3/public/scripts/app/bugTrackerApp/bugTrackerApp.js
+++ Day3/public/scripts/app/bugTrackerApp/bugTrackerApp.js
@@ -1,1 +1,15 @@
-angular.module('bugTrackerApp', ['utils']);
+angular.module('bugTrackerApp', ['utils','ngRoute']);
+angular
+			.module('bugTrackerApp')
+			.config(function($routeProvider){
+				$routeProvider
+					.when('/list', {
+						template : '<bugs></bugs>'
+					})
+					.when('/new', {
+						template : '<new-bug></new-bug>'
+					})
+					.otherwise({
+						redirectTo : '/list'
+					});
+			});
