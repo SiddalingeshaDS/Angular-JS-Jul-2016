33f26673608ab8aaedfe83b65092646327377c28
Siddalingesha D S <siddhudev813@gmail.com>
2016-07-22 12:23:17 +0530

    Server communication and Promises

---
+++ Day3/public/scripts/app/bugTrackerApp/utils/utils.js
@@ -0,0 +1,18 @@
+var utils = angular.module('utils', []);
+	utils.value('defaultTrimLength', 20);
+
+
+	utils.filter('trimText', function(defaultTrimLength){
+		return function(data, trimLength){
+			trimLength = trimLength || defaultTrimLength;
+			return data.length < trimLength ? data : String.prototype.substr.call(data,0,trimLength)+'...';
+		}
+	});
+
+    utils.value('momentApi', moment);
+
+    utils.filter('elapsed', function(momentApi){
+        return function(value){
+            return momentApi(value).fromNow();
+        }
+    });
